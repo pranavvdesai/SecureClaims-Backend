71dcce1cef650037b73b9cf8f333c8774e322918
Sahas Vivek <72225300+sahas-01@users.noreply.github.com>
2022-04-15 21:24:17 +0530

    feat: routes added

---
+++ routes/insuranceRoutes.js
@@ -0,0 +1,24 @@
+const express = require("express");
+
+const router = express.Router();
+
+const insuranceController = require("../controllers/insuranceController");
+
+const payementController = require("../controllers/payementController");
+// Post Request to store user Details
+
+router.get("/",(req,res,next)=>{
+    res.send("Welcome to Medical Insurance API")
+    })
+
+router.post("/insurance", insuranceController.postInsurance);
+
+router.get("/insurance", insuranceController.getAllInsurance);
+
+router.get("/insurance/:id", insuranceController.getInsurance);
+
+router.patch("/insurance/:id", insuranceController.patchInsurance);
+
+router.delete("/insurance/:id", insuranceController.deleteInsurance);
+
+module.exports = router;
